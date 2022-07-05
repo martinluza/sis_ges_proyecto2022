89631312dc52cb33a77fb894480d545575bcee88
hisokam324 <108706867+hisokam324@users.noreply.github.com>
2022-07-05 19:23:57 -0300

    Update UsuarioPersistencia.java

--- src/sis_ges_proyecto2022/persistencia/UsuarioPersistencia.java
+++ src/sis_ges_proyecto2022/persistencia/UsuarioPersistencia.java
@@ -23,8 +23,8 @@ public class UsuarioPersistencia {
 
     private static final String PS_SELECT_USUARIO = "SELECT  FROM usuarios where nombre=? and clave=?";
     // private static final String PS_UPDATE_USUARIO = "UPDATE db_sis_ges_proyecto2022.usuarios SET apellido = '?' WHERE (nombre = '?')";
-    private static final String PS_INSERT_USUARIO = "INSERT INTO db_sis_ges_proyecto2022.usuarios (nombre, clave) VALUES (?, ?)";
-    private static final String PS_SELECT_LISTA_USUARIO = "SELECT * FROM usuarios";
+    private static final String PS_INSERT_USUARIO = "INSERT INTO db_sis_ges_proyecto2022.usuarios (nombre, clave, estado) VALUES (?, ?, ?)";
+    private static final String PS_SELECT_LISTA_USUARIO = "SELECT  FROM usuarios where usuario=activo";
 
     public static void ingresarUsuario(Usuario usuario) throws UsuarioException {
 
@@ -38,6 +38,7 @@ public class UsuarioPersistencia {
             ps.setString(1,usuario.getNombre() );
             //ps.setString(2, usuario.getApellido());
             ps.setString(2, usuario.getClave());
+            ps.setString(3, "activo");
 
             ps.executeUpdate();
 
@@ -68,6 +69,7 @@ public class UsuarioPersistencia {
             con = conexion.conectar();
             ps = con.prepareStatement(PS_SELECT_LISTA_USUARIO);
             rs = ps.executeQuery();
+            rs.getString("nombre");
         
         } catch (SQLException e){
             throw new UsuarioException("Error");
@@ -118,34 +120,33 @@ public class UsuarioPersistencia {
         //paso 1 : crear la conexion a la base
         //paso 2 : crear el prepare statement
         //paso 3 : ejecutar la consulta del preparestatement
-        //paso 5 : cerrar la conexion a la base
-        
-        Conexion con = new Conexion();
-        String nombre = usuario.getNombre();
-        String clave = usuario.getClave();
-
-        PreparedStatement ps = null;
-
-        ResultSet rs = null;
-        try {
-            Connection conexion = con.conectar();
-            String sqlStm = "select * from db_sis_ges_proyecto2022.usuarios where nombre='" + nombre + "' and clave='" + clave + "';";
-            ps = conexion.prepareStatement(sqlStm);
-            rs = ps.executeQuery();
-
-        } catch (SQLException e) {
-
-        }
-
-        
+        //paso 5 : cerrar la conexion a la base    
     }
 
-    public void bajaUsuario(Usuario usuario) {
+    public void bajaUsuario(Usuario usuario) throws UsuarioException {
 
         //paso 1 : crear la conexion a la base
         //paso 2 : crear el prepare statement
         //paso 3 : ejecutar la consulta del preparestatement
-        //paso 5 : cerrar la conexion a la base
+        //paso 5 : cerrar la conexion a la 
+       
+        PreparedStatement ps = null;
+        
+        String nombre = usuario.getNombre();
+        String clave = usuario.getClave();
+
+        Conexion conexion = new Conexion();
+        Connection con = null;
+        try {
+            con = conexion.conectar();
+            String sqlStm = "UPDATE db_sis_ges_proyecto2022.usuarios SET estado = 'inactivo' WHERE (nombre = '"+nombre+"' and clave = '"+clave+"')";
+            ps = conexion.prepareStatement(sqlStm);
+            ps.executeUpdate();
+            
+        } catch (SQLException e) {
+            throw new UsuarioException("Error");
+        }
+        
     }
 
     public void modificacionUsuario(Usuario usuario) {
